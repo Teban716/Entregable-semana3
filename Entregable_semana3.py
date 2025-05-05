44ccefff69dda7901cc0d2cded00ebd4960b3301
Esteban Gonzalez <118945397+Teban716@users.noreply.github.com>
2025-05-04 20:21:21 -0500

    Add files via upload

--- Entregable_semana3.py
+++ Entregable_semana3.py
@@ -1,5 +1,16 @@
 inventario=[]
 
+def pedir_numero_positivo(prompt):
+    while(True):
+        try:
+            numero = float(input(prompt))
+            if numero > 0:
+                return numero
+            else:
+                print("El numero no es positivo")
+        except ValueError:
+            print("El numero es invalido")
+
 def menu_principal():
     print("1. Agregar productos")
     print("2. Consultar productos")
@@ -10,45 +21,63 @@ def menu_principal():
     opcion = input('Elige lo que deseas realizar:    ')
     if opcion == '1':
         agregar_producto()
+        menu_principal()
     elif opcion == '2':
-        print(inventario)
-        menu_principal
+        consultar_productos()
+        menu_principal()
     elif opcion == '3':
         actualizar_precios()
+        menu_principal()
     elif opcion == '4':
         eliminar_productos()
+        menu_principal()
     elif opcion == '5':
-        inventario()
+        calcular_valor_total_iventario()
+        menu_principal()
     elif opcion == "6":
         SystemExit
     else:
         print('Numero Invalido')
         print('-----------------------------------------------')
+        menu_principal()
 
 def agregar_producto():
-    producto = input("Producto a agregar:    ")
-    precio = float (input("Precio del producto:  "))
-    inventario.append(producto,precio)
-    pedir_otro_producto
+    producto = obtener_producto()
+    inventario.append(producto)
 
-def pedir_otro_producto():
-    añadir_producto=input("¿Deseas agregar otro producto? ")
-    if pedir_otro_producto==("si"):
-        añadir_producto
-    elif pedir_otro_producto == ("no"):
-        menu_principal
-    else: 
-        print("Respuesta invalida, escribir si o no")
-        pedir_otro_producto()    
+def obtener_producto():
+    nombre_producto = input("Producto:    ")
+    precio = pedir_numero_positivo("Precio:  ")
+
+    producto = {
+        'Nombre':nombre_producto,
+        'Precio':precio
+    }
+    
+    return producto
+
+def consultar_productos():
+    print(inventario)
+    
 
 def actualizar_precios():
-    input("")
+    producto_actualizar = obtener_producto()
+
+    for i, producto in enumerate(inventario):
+        if producto['Nombre'] == producto_actualizar['Nombre']:
+            inventario[i] = producto_actualizar
 
 def eliminar_productos():
-    input("")
+    nombre_producto=input("Producto a eliminar: ")
+    for producto in inventario:
+        if producto['Nombre'] == nombre_producto:
+            inventario.remove(producto)
 
 def calcular_valor_total_iventario():
-    print("")
+    calculadora = 0
+    for producto in inventario:
+        calculadora = calculadora + producto['Precio']  
+    print (calculadora)
 
 menu_principal()
     
