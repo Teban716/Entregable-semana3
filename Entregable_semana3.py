d04ff82daa73d62d9afd53946cd42586632c9862
Esteban Gonzalez <118945397+Teban716@users.noreply.github.com>
2025-05-07 21:49:17 -0500

    Add files via upload

--- Entregable_semana3.py
+++ Entregable_semana3.py
@@ -11,6 +11,17 @@ def pedir_numero_positivo(prompt):
         except ValueError:
             print("El numero es invalido")
 
+def pedir_numero_entero_positivo(prompt):
+    while(True):
+        try:
+            numero = int(input(prompt))
+            if numero > 0:
+                return numero
+            else:
+                print("El numero no es positivo")
+        except ValueError:
+            print("El numero es invalido")
+
 def agregar_producto():
     producto = obtener_producto()
     inventario.append(producto)
@@ -18,26 +29,30 @@ def agregar_producto():
 def obtener_producto():
     nombre_producto = input("Producto:")
     precio = pedir_numero_positivo("Precio:  $")
-    cantidad_disponible = pedir_numero_positivo("Cantidad Disponible:   ")
-
+    cantidad_disponible = (pedir_numero_entero_positivo("Cantidad Disponible:   "))
+    
     producto = {
             'Nombre':nombre_producto,
             'Precio':precio,
-            'Cantidad Disponible':cantidad_disponible
+            'Cantidad Disponible':(cantidad_disponible)
     }
     
     return producto
 
 def consultar_productos():
-    print(inventario)
+    if len(inventario) == 0:
+        print("El inventario esta vacio")
+    else:
+        print(f"CONSULTAR PRODUCTOS \n${inventario}")
     
-
 def actualizar_precios():
     producto_actualizar = obtener_producto()
-
-    for i, producto in enumerate(inventario):
-        if producto['Nombre'] == producto_actualizar['Nombre']:
-            inventario[i] = producto_actualizar
+    if len(inventario) == 0:
+        print("El inventario esta vacio")
+    else:
+        for i, producto in enumerate(inventario):
+            if producto['Nombre'] == producto_actualizar['Nombre']:
+                inventario[i] = producto_actualizar
 
 def eliminar_productos():
     nombre_producto=input("Producto a eliminar: ")
@@ -46,12 +61,14 @@ def eliminar_productos():
             inventario.remove(producto)
 
 def calcular_valor_total_iventario():
-    calculadora = 0
     for producto in inventario:
-        calculadora = calculadora + producto['Precio']  
-    print (calculadora)
-
-
+        multiplicacion = lambda inventario:({(inventario[producto]['Precio'])*(inventario[producto]['Cantidad Disponible'])})()
+        
+    # calculadora = 0
+    # for producto in inventario:
+    #     calculadora = calculadora + ((producto['Precio'])*(producto['Cantidad Disponible']))  
+    # print(f"VALOR TOTAL DEL INVENTARIO \n${calculadora}")
+    print(inventario)
 def menu_principal():
     while True:
         print('-----------------------------------------------')
@@ -59,7 +76,7 @@ def menu_principal():
         print("2. Consultar productos")
         print("3. Actualizar precios")
         print("4. Eliminar productos")
-        print("5. Inventario total")
+        print("5. Valor total del Inventario")
         print("6. Salir")
         print('-----------------------------------------------')
         opcion = input('Elige lo que deseas realizar:    ')
